95b3f43451d5b5756f657cba168a19a3fd8d32b5
saurabh53 <saurabh.arora53@live.com>
2021-03-24 16:05:58 +0530

    block reward

--- src/views/Home/components/CakeStats.tsx
+++ src/views/Home/components/CakeStats.tsx
@@ -47,7 +47,7 @@ const CakeStats = () => {
         </Row>
         <Row>
           <Text fontSize="14px">{TranslateString(540, 'New MANY/block')}</Text>
-          <CardValue fontSize="14px" decimals={0} value={5} />
+          <CardValue fontSize="14px" decimals={0} value={2.5} />
         </Row>
        
       </CardBody>
